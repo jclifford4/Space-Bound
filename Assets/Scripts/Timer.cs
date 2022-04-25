942929e06d44196febd92322a1168150889ae7d5
Raul Garcia <rgarcia5558@sdsu.edu>
2022-04-25 16:37:36 -0700

    Player Dead Scene Working

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78a1a04f8a13a1ba807f1d808089d73f6942960eb93044f83a9463da2d56bdc8
-size 1607
+oid sha256:b22b950c937d67d177d8ab311a6552d2a4635cd1030f3788e5811c1aa1496ac2
+size 1991
