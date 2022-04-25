dfc919b67d7a97711ae45c46f6944b19c5de1620
Raul Garcia <rgarcia5558@sdsu.edu>
2022-04-25 15:53:39 -0700

    Working Pause Menu

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e88b47e9812de1b0384bfde6fae920a51d7d1d8c31c7d4f58a634b6c0218a36e
-size 1608
+oid sha256:78a1a04f8a13a1ba807f1d808089d73f6942960eb93044f83a9463da2d56bdc8
+size 1607
--- Assets/Scripts/character_creator_controller.cs
+++ Assets/Scripts/character_creator_controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68889389fcd61ae17f87bbb913670e5dfe37e7438bd8a9670e56f9198c5c0bf9
-size 1275
+oid sha256:21a9257415aa558f02e89c5586fdaa7819e7fb6fec69e2fb2db230875d950c3e
+size 1273
