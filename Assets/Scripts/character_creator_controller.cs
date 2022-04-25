030b4355a0f666059020621ba200d777bb39b7a8
Raul Garcia <rgarcia5558@sdsu.edu>
2022-04-25 16:44:05 -0700

    Fix to Game Over Respawn

--- Assets/Scripts/character_creator_controller.cs
+++ Assets/Scripts/character_creator_controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f687c3aa8e0aab91cbf49840eb65c2de28d113c7c1e15873b8baf90ad717147
-size 1313
+oid sha256:4d08c8ec7c4953fbb77e9464098523a4795e8d064f4aeb10d3c868961b3327a5
+size 1364
