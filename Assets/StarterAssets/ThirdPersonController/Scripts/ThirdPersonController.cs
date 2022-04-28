12bbd63f1d29da5e3270cf2b461d2aeeb476c934
Gary Robards <grobards8259@sdsu.edu>
2022-04-27 19:54:12 -0700

    created ball dropper and started level 3 design

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bc0e6773d59fa141aa7191e61454c96525222734ec40df1bd89f75dde3ec346
-size 10725
+oid sha256:b296b5c2e50524e71f9b3ad520337a14043b99c000e96c65ec01185fa2dadfc4
+size 10729
