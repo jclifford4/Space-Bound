7ced8995bb9e0fb97d537fe8a49f1980b08bb180
Raul Garcia <rgarcia5558@sdsu.edu>
2022-05-01 08:05:17 -0700

    Before Merge

--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5706b993a6a19fd8bf9b8a56b8262e7da6c11ca754c60a9726798630e2b1bde7
-size 587
+oid sha256:c4b6f2df821fe9699f4a32165233791f25f7ad2ba80a34bfb62efa2fa9db28f1
+size 586
